e82f17a4a72daf8bce33fdf292f1ec7800693850
kelsy michael gagnebin <kelsymichael@gmail.com>
2015-04-28 15:25:39 -0600

    Add saveGiph() -> giphyObj: toDo-store saved

--- app.js
+++ app.js
@@ -27,36 +27,46 @@ app
 		var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 	    $scope.day = dayNames[new Date().getDay()];		
 		
-		var giphyArr = [];
-	
-		$scope.searchGiphy = function(uInput){
-			$scope.urlInput = uInput;
-
-			giphyService.getGiphy(uInput).then(function(response){
-				console.log(response);
-				var ranNumber = Math.floor(Math.random() * (25 +1 ));
-
-				$scope.giph = response[ranNumber].images.fixed_height.url;
-				console.log(response[ranNumber].images.fixed_height.url);
-			});
-
-			ranNumber = '';
-
-			console.log(uInput);
-			$scope.uInput = '';
-			console.log($scope.urlInput);
+		$scope.giphyArr = [];
+		$scope.giphyObj = {
+			pic: '',
+			url: ''
 		};
 
+		$scope.searchGiphy = function(uInput){
+			$scope.urlInput = uInput;
+			giphyService.getGiphy(uInput).then(function(response){
+				// console.log(response);
+				var ranNumber = Math.floor(Math.random() * (25 +1 ));
+				
+				$scope.giph = response[ranNumber].images.fixed_height.url;
+				$scope.giphS = response[ranNumber].images.fixed_height_still.url;
+				// console.log(response[ranNumber].images.fixed_height.url);
+			
+			// save-giph button //	
+			$scope.saveGiphy = function(){
+				$scope.giphyArr.push($scope.giphS);
+				$scope.giphyObj.pic = $scope.giphS;
+				$scope.giphyObj.url = $scope.giph;
+				// console.log($scope.giphyArr);
+				console.log($scope.giphyObj.pic);
+			};
+		});
 
+			// ranNumber = '';
+			// // console.log(uInput);
+			// $scope.uInput = '';
+			// // console.log($scope.urlInput);
+			// $scope.giphyObj = ''
+		};
 
+		// $scope.saveGiphy = function(){
+		// 	giphyArr.push($scope.searchGiphy(uInput));
+		// 	console.log(giphyArr);
+		// };
+	
 
-	// this.getGiphy = function(uInput){
-	// 	$http.get('https://giphy.p.mashape.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=' + uInput, config1)
-	// 		.success(function(data){
-	// 			$scope.giph = data;
-	// 		});
-
-});
+}); /* end of controller */
 
 app
 	.service('giphyService', function($http, $q){
@@ -64,7 +74,6 @@ app
 		var config1 = {headers: {
 			'X-Mashape-Key' : 'Vt18ObzVHymshu7BgkVs9OW36RWnp1J9Z5EjsnaqZM34gYbp3a',
 			'Accept' : 'application/json',
-			'limit' : '2'
 		}};	
 
 		this.getGiphy = function(uInput){
@@ -76,10 +85,11 @@ app
 
 				return deferred.promise;
 			// .success(function(data){
-
-			// });
 		};
-	});
+
+	
+
+	}); /* end of service */
 
 
 		
